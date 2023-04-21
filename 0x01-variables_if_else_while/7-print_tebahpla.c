a201718d8e086e03d89dd9ce16f783b6c30caaeb
AdebankeE <adebankepeke02@gmail.com>
2023-04-21 13:23:04 -0700

    programming im C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Program that prints the lowercase alphabet in reverse
+ * Return: 0;
+ */
+int main(void)
+{
+char c = 'z';
+
+while (c >= 'a')
+{
+	putchar(c);
+	c--;
+}
+putchar('\n');
+}
