738a1c2519b043c1ffa03c5bd21bd7b114c610bc
Adebank <adebankepeke04@gmail.com>
2023-04-25 13:22:30 +0000

    C-Functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ if (res >= 10)
 _putchar((res / 10) + '0');
 	_putchar((res % 10) + '0');
 }
-else if(res < 10 && j != 0)
+else if (res < 10 && j != 0)
 {
 _putchar(' ');
 _putchar((res % 10) + '0');
