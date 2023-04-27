e96b18b13ede7f8a1d1eeebc0fcb61a2e23e5c3a
Adebank <adebankepeke04@gmail.com>
2023-04-27 12:11:02 +0000

    functions and nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _is digit -  checks for digit 1 - 9
+ * @c: character to check.
+ * Return: 1 if c is digit, otherwise return 0.
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+	return (1);
+else
+	return (0);
+}
